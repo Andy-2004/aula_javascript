c9b2080b39d02586d734b3a19d7fbebcbf4c958e
Andy-2004 <54247945+Andy-2004@users.noreply.github.com>
2020-06-17 15:22:41 -0300

    Atualização

---
+++ main.js
@@ -0,0 +1,87 @@
+function clicou(){
+  document.getElementById("Agradecimento";
+ // console.log(document.getElementById("Agradecimento"));
+  
+    //  alert("Obrigado por clicar");
+}
+
+
+
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+*/
+
+/*
+var d = new Date();
+alert(d);
+*/
+
+
+
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+
+
+
+
+
+/*
+var count = 0;
+while (count < 5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+
+/*
+var idade = prompt("Qual sua idade");
+if (idade >= 18){
+    alert("Maior de idade"); 
+}else{
+    alert("Menor de idade");
+};
+*/
+
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+/*
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+lista.pop();
+
+console.log(lista[1]);
+
+
+
+var nome = "Claudemir Anderson";
+var idade = 21
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+alert(idade + idade2);
+alert(nome + " tem " + idade + " anos ");
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão", "Brasil"));
+*/
+
+
